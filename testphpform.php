6c6f628980182c746e4a7ef7e1e7192123a1be82
Charlene167 <charlene.corencin@edu.ece.fr>
2021-12-17 21:22:43 +0100

    commit commentaire php

--- testphpform.php
+++ testphpform.php
@@ -4,17 +4,21 @@
     $genre = isset($_POST["genre"])? $_POST["genre"] :"";
     $type = isset($_POST["type"])? $_POST["type"] :"";
 
+    //Connexion avec la base de données
+
     $database = "mineur";
 
     $db_handle = mysqli_connect('localhost', 'root', '');
     $db_found = mysqli_select_db($db_handle, $database);
+
     $db_handle->set_charset('utf8');
 
-
-
+    //Si le formulaire est validée
     if(isset($_POST["valider"])){
         if($db_found){
+            //Ajouter le nouvel utilisateur dans la base de données
             $sql = "INSERT INTO personne (Nom, Genre, theme) VALUES ('$nom','$genre', '$type')";
+            //Récupérer l'histoire choisi
             $sql2 = "SELECT * FROM paragraphes WHERE theme LIKE '%$type%'";
             
             $result = mysqli_query($db_handle, $sql);
@@ -24,6 +28,7 @@
         if (mysqli_num_rows($result2) == 0) {
             echo "RAS";
         }else{
+            //Affichage en modifiant le nom par défaut "Alex" par le nom de notre personnage
             while ($data = mysqli_fetch_assoc($result2)) {
                 echo "<h2 style ='text-align: center; font-family: cursive;'>Voici ton histoire " . $nom . ". Bonne lecture ! </h2>" . "<br>" . "<br>" . "<br>";
                 echo "<div style =' font-family: cursive; margin-left : 20px; margin-right = 100px;'>" . str_replace("Alex", $nom, $data['p1'] ) . "</div>" . "<br>" . "<br>";
@@ -39,7 +44,7 @@
 mysqli_close($db_handle);
 
 ?>
-
+<!--html de la page de l'histoire-->
 <!DOCTYPE html>
 <html>
     <head>
